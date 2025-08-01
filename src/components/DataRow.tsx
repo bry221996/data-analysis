7aa87cc44640d2aede23d43f56904b261b4b7523
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2025-08-01 17:28:46 +0800

    FIX

--- src/components/DataRow.tsx
+++ src/components/DataRow.tsx
@@ -8,9 +8,9 @@ interface Note {
 
 const DataRow = ({ day, record}: {day: number, record: Record}) => {
     const roasNote: Note = useMemo(() => {
-        if (record.roas >= 6) {
+        if (record.roas as number >= 6) {
             return { note: 'High ROAS', status: true }
-        } else if (record.roas >= 3 && record.roas < 6) {
+        } else if (record.roas as number >= 3 && record.roas as number < 6) {
             return { note: 'Good ROAS', status: true }
         } else {
             return { note: 'Low ROAS', status: false }
@@ -18,7 +18,7 @@ const DataRow = ({ day, record}: {day: number, record: Record}) => {
     }, [record.roas])
 
     const ctrNote: Note = useMemo(() => {
-        if (record.ctr >= 1.5) {
+        if (record.ctr as number >= 1.5) {
             return { note: 'High CTR', status: true }
         } else {
             return { note: 'Low CTR', status: false }
@@ -26,7 +26,7 @@ const DataRow = ({ day, record}: {day: number, record: Record}) => {
     }, [record.ctr])
 
     const cpmNote: Note = useMemo(() => {
-        if (record.ctr <= 15) {
+        if (record.ctr as number <= 15 && record.ctr as number > 0) {
             return { note: 'Good CPM', status: true }
         } else {
             return { note: 'Bad CPM', status: false }
@@ -34,7 +34,7 @@ const DataRow = ({ day, record}: {day: number, record: Record}) => {
     }, [record.ctr])
 
     const hookRateNote: Note = useMemo(() => {
-        if (record.hookRate >= 40) {
+        if (record.hookRate as number >= 40) {
             return { note: 'Good Hook', status: true }
         } else {
             return { note: 'Need to improve Hook', status: false }
@@ -42,7 +42,7 @@ const DataRow = ({ day, record}: {day: number, record: Record}) => {
     }, [record.hookRate])
 
     const msgConversionRateNote: Note = useMemo(() => {
-        if (record.messageConversationRate >= 40) {
+        if (record.messageConversationRate as number >= 40) {
             return { note: 'Good Greetings', status: true }
         } else {
             return { note: 'Need to improve greetings', status: false }
